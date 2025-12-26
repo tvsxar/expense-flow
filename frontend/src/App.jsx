d4f3c4403f1e58e95525d6d912eef46bf845825c
Taras Poiatsyka <tarasofficial82@gmail.com>
2025-12-26 16:25:06 +0200

    feat: added GET query on Dashboard, changed modal functionality, wrapped Dashboard into Provider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import DashboardPage from './pages/DashboardPage';
+import { ApolloProvider } from '@apollo/client';
+import client from './apollo/client';
 
 function App() {
 
   return (
-    <>
+    <ApolloProvider client={client}>
       <DashboardPage />
-    </>
+    </ApolloProvider>
   )
 }
 
