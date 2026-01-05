2573b46ee7b2ecf96fb523b0c5458ea7f0a13046
Taras Poiatsyka <tarasofficial82@gmail.com>
2026-01-05 13:40:38 +0200

    changes for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,13 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import path from "path";
+import { fileURLToPath } from "url";
 import expenseSchema from "./schemas/expenseSchema.js";
 import { createHandler } from "graphql-http/lib/use/express";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 
@@ -15,9 +20,17 @@ app.use(cors());
 app.use(express.json());
 app.use("/graphql", createHandler({ schema: expenseSchema, graphiql: true }));
 
-app.get("/", (req, res) => {
-  res.send("Expense Flow Backend is running");
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*splat}", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}/graphql`);
